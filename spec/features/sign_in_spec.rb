40c07091eba82c04065d8fd1848054b0214b0aea
Becky Osiecki <lobeliaushtar@gmail.com>
2014-03-14 11:42:32 -0400

    Rails II - Sign Out - complete

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -12,7 +12,7 @@ describe "Signing in" do
 		expect(page).to have_field("Email")
 		expect(page).to have_field("Password")
 	end
-
+	
 	it "signs in the user if the email/password combination is valid" do
 		user = User.create!(user_attributes)
 
@@ -25,14 +25,14 @@ describe "Signing in" do
 
 		click_button 'Sign In'
 
-		expect(current_path).to eq(user_path(user))	 
+		expect(current_path).to eq(user_path(user))
 
 		expect(page).to have_text("Welcome back, #{user.name}!")
 		expect(page).to have_link(user.name)
 		expect(page).not_to have_link('Sign In')
 		expect(page).not_to have_link('Sign Up')
 	end
-
+	
 	it "does not sign in the user if the email/password combination is invalid" do
 		user = User.create!(user_attributes)
 
@@ -44,7 +44,7 @@ describe "Signing in" do
 		fill_in "Password", with: "no match"
 
 		click_button 'Sign In'
-
+				
 		expect(page).to have_text('Invalid')
 		expect(page).not_to have_link(user.name)
 		expect(page).to have_link('Sign In')
